ebf03cd6b10ea1e2ccb1a6b6c6346a0dfc91a072
zh137 <yz456546@qq.com>
2023-12-29 09:12:05 +0800

    tijiao

---
+++ Demo2706/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo2706;
+
+class Program
+{
+    public int BuyChoco(int[] prices, int money)
+    {
+        prices = prices.Order().ToArray();
+        //Array.Sort(prices);
+        return money - prices[0] - prices[1] > 0 ? money - prices[0] - prices[1] : money;
+    } 
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Demo2735/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo2735;
+
+class Program
+{
+    public long MinCost(int[] nums, int x)
+    {
+        var sum = nums.Sum(i => (long)i);
+        var length = nums.Length;
+        var newNums = new long[length];
+        nums.CopyTo(newNums,0);
+        //这个是转动次数
+        for (long i = 0; i < length; i++)
+        {
+            // 计算当前的成本
+            for (long j = 0; j < length; j++)
+                newNums[j] = Math.Min(newNums[j], nums[(j + i) % length]);
+            sum = Math.Min(newNums.Sum() + i * x, sum);
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
