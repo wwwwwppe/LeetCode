6f24108a9912b543770eb758bf0750823a143468
zh137 <yz456546@qq.com>
2024-04-17 15:03:14 +0800

    更新

---
+++ Demo419/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Demo419;
+
+public class Program
+{
+    public int CountBattleships(char[][] board)
+    {
+        int m = board.Length;
+        int n = board[0].Length;
+        int num = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == 'X')
+                {
+                    num++;
+                    if (j < n - 1 && board[i][j + 1] == 'X')
+                    {
+                        int k = j + 1;
+                        while (k < n && board[i][k] == 'X')
+                        {
+                            board[i][k] = '.';
+                            k++;
+                        }
+                    }
+                    else if (i < m - 1)
+                    {
+                        int k = i + 1;
+                        while (k < m && board[k][j] == 'X')
+                        {
+                            board[k][j] = '.';
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return num;
+    }
+
+    public static void Main(string[] args)
+    {
+    }
+}
---
+++ Demo598/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo598;
+
+public class Program {
+
+    public int MaxCount(int m, int n, int[][] ops)
+    {
+        if(ops.Length==0) return m*n;
+        int a = ops.Min(x => x[0]);
+        int b = ops.Min(x => x[1]);
+        return a *b;
+        
+//        int max = a.Max(x => x.Max());
+//        return a.Sum(x => x.Count(c => c==max));
+    }
+    
+    public static void Main(string[] args)
+    {
+        
+    }
+}
