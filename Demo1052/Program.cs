e2b02315deae57c645c8d0a020093f1409216834
zh137 <yz456546@qq.com>
2024-04-24 13:17:02 +0800

    日常更新

---
+++ Demo1052/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Demo1052;
+
+class Program
+{
+    public int MaxSatisfied(int[] customers, int[] grumpy, int minutes)
+    {
+        int nums = 0;
+        if (customers.Length != grumpy.Length)
+        {
+            return 0;
+        }
+
+        for (var i = 0; i < customers.Length; i++)
+        {
+            if (grumpy[i] == 1) continue;
+            nums += customers[i];
+            customers[i] = 0;
+        }
+
+        int left = 0, right = minutes-1;
+        int max = 0,trueMax = 0;
+        for (int i = 0; i <= right; i++)
+        {
+            max += customers[i];
+        }
+
+        trueMax = max;
+        while (right<customers.Length-1)
+        {
+            max -= customers[left];
+            right++;
+            left++;
+            max += customers[right];
+            trueMax = Math.Max(trueMax, max);
+        }
+
+        return nums + trueMax;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Demo707/MyLinkedList.cs
@@ -0,0 +1,49 @@
+﻿namespace Demo707;
+
+public class MyLinkedList
+{
+    class ListNode
+    { 
+        int val;
+        ListNode next;
+        ListNode prev;
+
+        internal ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    private int size;
+    private ListNode dummyHead, dummyTail;
+    
+    public MyLinkedList()
+    {
+        size = 0;
+        dummyHead = new ListNode(0);
+        dummyTail = new ListNode(0);
+        
+        
+    }
+    
+    public int Get(int index)
+    {
+        
+    }
+    
+    public void AddAtHead(int val) {
+        
+    }
+    
+    public void AddAtTail(int val) {
+
+    }
+    
+    public void AddAtIndex(int index, int val) {
+
+    }
+    
+    public void DeleteAtIndex(int index) {
+
+    } 
+}
