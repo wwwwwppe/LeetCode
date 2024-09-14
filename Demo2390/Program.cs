15b097e38e1251ac95fee92ea2a29d6cb60c7e2c
ubuntu <ubuntu@localhost.localdomain>
2024-09-14 14:35:24 +0800

    leetcode中超过82%的用户

--- Demo2390/Program.cs
+++ Demo2390/Program.cs
@@ -2,8 +2,33 @@
 
 class Program
 {
+    public string RemoveStars(string s)
+    {
+        var len = 0;
+        List<char> list = [];
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == '*')
+            {
+                len++;
+            }
+            else if (len == 0)
+            {
+                list.Add(s[i]);
+            }
+            else
+            {
+                len--;
+            }
+        }
+        list.Reverse();
+        return new string(list.ToArray());
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        String s = "leet**cod*e";
+        Program program = new Program();
+        System.Console.WriteLine(program.RemoveStars(s));
     }
 }
