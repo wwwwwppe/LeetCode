9cf3b0f14577ba9fe691a5cb57c626023f3b9d54
ubuntu <ubuntu@localhost.localdomain>
2024-09-19 13:24:32 +0800

    Demo2414已AC，超过100%

---
+++ Demo2414/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo2414;
+
+class Program
+{
+    public int LongestContinuousSubstring(string s)
+    {
+        int left = 0,
+            right = 1;
+        int max = 1;
+        var cs = s.ToCharArray();
+        while (right < cs.Length)
+        {
+            if (cs[right] - cs[right-1] == 1)
+            {
+                max = Math.Max(max, right - left + 1);
+            }
+            else
+            {
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
