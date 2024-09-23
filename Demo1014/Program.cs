6d087c34fa06f21c81c2a9369eebc251d1358cd9
ubuntu <ubuntu@localhost.localdomain>
2024-09-23 11:08:45 +0800

    Demo1014已AC

---
+++ Demo1014/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo1014;
+
+class Program
+{
+    public int MaxScoreSightseeingPair(int[] values)
+    {
+        int max = 0,
+            left = 0,
+            right = 1;
+        while (right < values.Length)
+        {
+            max = Math.Max(max, values[right] + values[left] + left - right);
+            if (values[left] - values[right] < right - left)
+            {
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
