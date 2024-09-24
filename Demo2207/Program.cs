4e19668236cfe132fbe98d88296cf12779d29c61
ubuntu <ubuntu@localhost.localdomain>
2024-09-24 10:10:23 +0800

    Demo2207 等待DeBug

---
+++ Demo2207/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Demo2207;
+
+class Program
+{
+    public long MaximumSubsequenceCount(string text, string pattern)
+    {
+        int a = text.IndexOf(pattern[0]);
+        int b = text.LastIndexOf(pattern[1]);
+        List<long> lists = [];
+        if (a == -1 && b == -1)
+            return 0;
+        if (a == -1)
+            return text.Count(c => c == pattern[1]);
+        if (b == -1)
+            return text.Count(c => c == pattern[0]);
+
+        //这里要做处理,如果缺少一边的话，不一定为0
+        bool aa = true;
+        long temp = 0;
+        for (var i = a; i <= b; i++)
+        {
+            if ((text[i] == pattern[0] && aa) || (text[i] == pattern[1] && !aa))
+            {
+                temp++;
+            }
+            else if ((text[i] == pattern[0] && !aa) || (text[i] == pattern[1] && aa))
+            {
+                aa = !aa;
+                lists.Add(temp);
+                temp = 1;
+            }
+        }
+        if (temp != 0)
+            lists.Add(temp);
+        long preLeft = 0,
+            preRigth = 0;
+        for (int i = 0; i < lists.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                preLeft += lists[i];
+            }
+            else
+            {
+                preRigth += lists[i];
+            }
+        }
+
+        //两边都加一，看看那边最后结果大
+        preLeft++;
+        preRigth++;
+
+        long ret1 = 0,
+            ret2 = 0;
+
+        for (int l = 0; l < lists.Count; l++)
+        {
+            if (l % 2 == 0)
+            {
+                ret1 += lists[l] * preRigth;
+            }
+            else
+            {
+                preRigth -= lists[l];
+            }
+        }
+
+        for (int i = lists.Count - 1; i >= 0; i--)
+        {
+            if (i % 2 == 1)
+            {
+                ret2 += lists[i] * preLeft;
+            }
+            else
+            {
+                preLeft -= lists[i];
+            }
+        }
+
+        return Math.Max(ret1, ret2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
