4c9bd826b971f0e589c114317dca758331c1141b
ubuntu <ubuntu@localhost.localdomain>
2024-10-17 09:21:05 +0800

    Demo3162

---
+++ Demo3162/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo3162;
+
+class Program
+{
+    public int NumberOfPair(int[] nums1, int[] nums2, int k)
+    {
+        int ret = 0;
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            for (int j = 0; j < nums2.Length; j++)
+            {
+                int demo = nums2[j] * k;
+                if (demo == 0)
+                    continue;
+                if (nums1[i] % demo == 0)
+                    ret++;
+            }
+        }
+        return ret;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
