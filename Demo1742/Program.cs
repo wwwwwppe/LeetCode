1c26e0ec872016224c5d52c34f5e64cabfc23496
yize <yize@123.com>
2025-02-13 10:13:42 +0800

    Demo1742初步代码写入

--- Demo1742/Program.cs
+++ Demo1742/Program.cs
@@ -2,6 +2,24 @@
 
 class Program
 {
+    /*
+     *  想一下思路
+     *  这是一个简单题，所以不难，但是现在的我太久没写了，所以需要来想一想
+     *  2025-2-13 9:56
+     *  最简单的方法，写一个方法，单独算出的方法，然后再一个循环
+     *  我先来考一下
+     * */
+    public int CountBalls(int lowLimit,int highLimmit){
+      
+    }
+
+    int getSameNums(nums){
+      while (nums / 10 > 0)
+      {
+        
+      }
+    }
+  
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
