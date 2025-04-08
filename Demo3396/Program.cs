923e886054f9e0f86574ed0241aaf8ce1bfab50e
yize <yize@123.com>
2025-04-08 11:10:40 +0800

    Demo3396格式化

---
+++ Demo3396/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo3396;
+
+class Program
+{
+    public int MinimumOperations(int[] nums){
+      var set = new HashSet<int>;
+      var numsLong = nums.Count();
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
